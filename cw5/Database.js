850f79a19add402045c2c7c4fee8b5274751c9bf
ecenuryildiz <41239714+ecenuryildiz@users.noreply.github.com>
2019-11-06 10:49:05 +0300

    Add files via upload

---
+++ cw5/Database.js
@@ -0,0 +1,23 @@
+class Course{
+    constructor(id,time,date,course){
+        this.id=id;
+        this.time=time;
+        this.date=date;
+        this.course=course;
+    }
+    toString(){
+        return "Course's id is "+this.id;
+    }
+}
+
+class Student{
+    constructor(id,name,gpa,course){
+        this.id=id
+        this.name=name
+        this.gpa=gpa
+        this.course=course
+    }
+    toString(){
+        return "Student's id is "+this.id;
+    }
+}
